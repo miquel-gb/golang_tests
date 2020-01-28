6417e5c35df2ab952f9687f0d3beed15fbff4438
Miquel GB <mgines@bbsoftbalear.com>
2020-01-28 15:35:18 +0100

    Methods tests

--- maps_ex/maps.go
+++ maps_ex/maps.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type Vertex struct {
 	Lat, Long float64
@@ -8,10 +11,32 @@ type Vertex struct {
 
 var m map[string]Vertex
 
+func WordCount(s string) map[string]int {
+	wordMap := make(map[string]int)
+	words := strings.Fields(s)
+	for _, w := range words {
+		wordMap[w]++
+	}
+	return wordMap
+}
+
+func fibonacci() func() int {
+	num1, num2 := 0, 1
+	return func() int {
+		num1, num2 = num2, num1+num2
+		return num1
+	}
+}
+
 func main() {
-	m = make(map[string]Vertex)
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+	/*fmt.Println(WordCount("I am I Go! am learning Go!"))*/
+	/*m = make(map[string]Vertex)
 	m["Bell Labs"] = Vertex{
 		40.68433, -74.39967,
 	}
-	fmt.Println(m["Bell Labs"])
+	fmt.Println(m["Bell Labs"])*/
 }
