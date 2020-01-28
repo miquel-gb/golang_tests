0711e8c3e70cf02c9da80c636bfced3dc2f85e71
Miquel GB <mgines@bbsoftbalear.com>
2020-01-28 10:01:36 +0100

    Maps exercises creation, more loops exercises

--- loops_ex/loops.go
+++ loops_ex/loops.go
@@ -5,6 +5,22 @@ import (
 	"math"
 )
 
+func Pic(dx, dy int) [][]uint8 {
+	// initialize the image
+	image := make([][]uint8, dy)
+
+	// iterate over each pixel
+	for x := 0; x < dy; x++ {
+		image[x] = make([]uint8, dx)
+		for y := 0; y < dx; y++ {
+			// set a color value for each pixel
+			image[x][y] = uint8((x ^ y) * (x ^ y))
+
+		}
+	}
+	return image
+}
+
 func Sqrt(x float64) float64 {
 	z := 1.0
 	prevZ := z
@@ -39,7 +55,7 @@ func pow(x, n, lim float64) float64 {
 }
 
 func main() {
-	fmt.Println("Qué pasha")
+	/*fmt.Println(Pic())*/
 	/*forAsWhile()*/
 
 }
